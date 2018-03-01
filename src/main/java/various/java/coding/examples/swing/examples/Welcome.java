16d9653e2f9f46531b6b83e6f97aa6a4bc1719f5
innuendo <corda.ilaria@gmail.com>
2018-03-01 14:05:35 +0000

    ADD some Swing login form example as per here:
    http://www.oodlestechnologies.com/blogs/Create-login-form-in-Java-using-Swing

---
+++ src/main/java/various/java/coding/examples/swing/examples/Welcome.java
@@ -0,0 +1,12 @@
+package various.java.coding.examples.swing.examples;
+
+import javax.swing.*;
+class Welcome extends JFrame
+{
+    Welcome()
+    {
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Welcome");
+        setSize(400, 200);
+    }
+}
